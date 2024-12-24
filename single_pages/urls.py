fd7cedc45cfecb1c0184e81b68d7042439ac9a76
SangjinKO <kosang234@gmail.com>
2024-12-24 15:09:36 +0900

    add sitemap

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -1,9 +1,12 @@
 
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('about_me/', views.about_me),
     path('my_flix/', views.my_flix),
-    path('', views.landing)
+    path('', views.landing),
+    path('sitemap/', TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
+
 ]
